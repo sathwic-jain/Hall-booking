f191c5be3162816911001fb009f9e95c5adfb574
Sathwic Jain <sathwic.j@gmail.com>
2022-01-08 13:11:24 +0530

    first

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
+import cors from "cors";
 const app=express();
 app.use(express.json());
-
+app.use(cors());
 const PORT=process.env.PORT || 6000;
 app.listen(PORT,()=>console.log("listening to",PORT));
 
